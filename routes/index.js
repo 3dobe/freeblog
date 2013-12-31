7481a6f4141094bb86016d6418bd3debb84ff0c6
Fritz-Lium <fritzlium@gmail.com>
2013-12-31 23:05:01 +0800

    add views/public/routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
-	app.get('/', function(req, res){
-		res.render('index', {
+	app.get('/', function (req, res) {
+		res.render('home', {
 			title: 'freeblog'
 		});
 	});
