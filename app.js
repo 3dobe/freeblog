4f29e766308bd78216f20e15ef28820a060c4e2e
Fritz-Lium <fritzlium@gmail.com>
2014-01-01 17:17:42 +0800

    error handling

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
+// app environment
+// development | test | production
+if (!process.env['NODE_ENV']) {
+	var env = process.argv && process.argv[2] || 'development';
+	process.env['NODE_ENV'] = env;
+}
+// app title
 process.title = 'freeblog';
 
 var express = require('express'),
-	lessMiddleware = require('less-middleware'),
 	routes = require('./routes'),
 	config = require('./config'),
 	port = config.port,
-	secret = config.secret,
-	viewDir = config.viewDir,
 	publicDir = config.publicDir,
 	app = express();
 
-// all environments
-app.set('views', viewDir);
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser(secret));
-app.use(express.session());
-app.use(lessMiddleware({
-	src: publicDir
-}));
-app.use(express.static(publicDir));
+app.configure(function () {
+	app.set('views', config.viewDir);
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser(config.secret));
+	app.use(express.session());
+	app.use(require('less-middleware')({ src: publicDir }));
+	app.use(app.router);
+	app.use(express.static(publicDir));
+});
 
-// development only
-if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
-}
+// error handling
+app.configure(function () {
+	app.configure('development', function () {
+		// already including error logging
+		app.use(express.errorHandler());
+	});
+	app.configure('production', function () {
+		app.use(function (err, req, res, next) {
+			// 4 parameters required to takie in error
+			console.error(err.stack);
+			res.send(500);
+		});
+	});
+});
 
 // routes
 routes(app);
