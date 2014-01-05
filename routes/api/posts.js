e0778151677aca80249148b75990811382644bdb
funkyHui <540886643@qq.com>
2014-01-05 14:24:07 +0800

    modify posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,69 +4,73 @@ var Post = require('../../models/post'),
 module.exports = function(app){
 	//add post
 	app.post('/api/posts', function(req, res){
-		var post = new Post(req.body),
-			message,
-			url;
-		post.save(function(err, p) {
-			if(err){
-				message = err.message;
-				url = '/admin';
-			} else {
-				message = 'Success';
-				url = '/posts/' + p.id;
-			}
-		});
-		res.cookie(
-			'message',
-			message,
-			{ httpOnly: false }
-		);
-		res.redirect(url);
-	});
-
-	//delete post by id
-	app.delete('/api/posts/:id', function(req, res) {
-		var id = parseInt(req.body['id']),
-			url = '/admin',
-			message;
-		Post.findByIdAndRemove(id, function(err, post) {
-			if (!post) {
-				message = 'No such post';
-			} else {
-				post.remove();
-				message = 'Post deleted';
-			}
-		});
-		res.cookie(
-			'message',
-			message,
-			{ httpOnly: false }
-		);
-		res.redirect(url);
+		if (!req.user) {
+			res.cookie('message', 'Auth fail', { httpOnly: false });
+			res.redirect('/albums');
+		} else {
+			var post = new Post(req.body),
+				message,
+				url;
+			post.save(function(err, p) {
+				if(err){
+					message = err.message;
+					url = '/admin';
+				} else {
+					message = 'Success';
+					url = '/posts/' + p.id;
+				}
+			});
+			res.cookie('message', message, { httpOnly: false });
+			res.redirect(url);
+		}
 	});
 
 	//update post by id
 	app.put('/api/posts/:id', function(req, res) {
-		var id = parseInt(req.body['id']),
-			name = req.body['name'],
-			body = req.body['body'],
-			url,
-			message;
-		Post.findByIdAndUpdate(id, { $set: { name: req.body['name'], body: req.body['body'] }}, function(err, post) {
-			if (!post) {
-				message = 'No such post';
-				url = '/admin'
-			} else {
-				message = 'Post updated'
-				url = '/posts/' + id;
-			}
-		});
-		res.cookie(
-			'message',
-			message,
-			{ httpOnly: false }
-		);
-		res.redirect(url);
+		if (!req.user) {
+			res.cookie('message', 'Auth fail', { httpOnly: false });
+			res.redirect('/albums');
+		} else {
+			var id = parseInt(req.body['id']),
+				url,
+				message;
+			Post.findByIdAndUpdate(id, {
+				$set: req.body
+			}, function(err, post) {
+				if (!post) {
+					message = 'No such post';
+					url = '/admin'
+				} else {
+					message = 'Post updated'
+					url = '/posts/' + id;
+				}
+			});
+			res.cookie('message', message, { httpOnly: false });
+			res.redirect(url);
+		}
+	});
+
+	//delete post by id
+	app.delete('/api/posts/:id', function(req, res) {
+		if (!req.user) {
+			res.cookie('message', 'Auth fail', { httpOnly: false });
+			res.redirect('/albums');
+		} else {
+			var id = parseInt(req.body['id']),
+				url = '/admin',
+				message;
+			Post.findByIdAndRemove(id, function(err, post) {
+				if (!post) {
+					message = 'No such post';
+				} else {
+					post.remove();
+					message = 'Post deleted';
+				}
+			});
+			res.cookie('message', message, { httpOnly: false });
+			res.redirect(url);
+		}
+
 	});
 
 	//add comment
@@ -87,39 +91,36 @@ module.exports = function(app){
 				url = '/posts/' + id;
 			}
 		});
-		res.cookie(
-			'message',
-			message,
-			{ httpOnly: false }
-		);
+		res.cookie('message', message, { httpOnly: false });
 		res.redirect(url);
 	});
 
 	//delete comment
 	app.delete('/api/posts/:pid/comments/:cix', function(req, res){
-		var pid = parseInt(req.params['pid']),//post id
-			cindex = parseInt(req.params['cix']),//comment id
-			url = '/admin',
-			message;
-		Post.findByIdAndRemove(pid, function(err, post){
-			if(!post) {
-				message = 'No such post';
-			} else {
-				post.deleteComment(cindex, function(err){
-					if(err) {
-						message = err.message;
-					} else {
-						message = 'Comment deleted';
-					}
-				});
-			}
-		});
-		res.cookie(
-			'message',
-			message,
-			{ httpOnly: false }
-		);
-		res.redirect(url);
+		if (!req.user) {
+			res.cookie('message', 'Auth fail', { httpOnly: false });
+			res.redirect('/albums');
+		} else {
+			var pid = parseInt(req.params['pid']),//post id
+				cindex = parseInt(req.params['cix']),//comment id
+				url = '/admin',
+				message;
+			Post.findByIdAndRemove(pid, function(err, post){
+				if(!post) {
+					message = 'No such post';
+				} else {
+					post.deleteComment(cindex, function(err){
+						if(err) {
+							message = err.message;
+						} else {
+							message = 'Comment deleted';
+						}
+					});
+				}
+			});
+			res.cookie('message', message, { httpOnly: false });
+			res.redirect(url);
+		}
 	});
 
 };
